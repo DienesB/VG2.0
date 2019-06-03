ce127d1a5935dbedae9075ae4f616f9102d9c060
Balazs Dienes <dienes.balazs88@gmail.com>
2019-06-03 12:26:51 +0200

    Update inputCDSM_creator.py

--- SEBEphen/inputCDSM_creator.py
+++ SEBEphen/inputCDSM_creator.py
@@ -1,7 +1,7 @@
 # Author: Balazs Dienes
-# contact: dienes.balazs88@gmail.com
+# Contact: dienes.balazs88@gmail.com
 
-# This file sets all CDSM values to 0 if they do not represent the vegtype with which SEBE is running in this round.
+# This file sets all canopy digital surface model (CDSM) values to 0 if they do not represent the vegtype with which SEBE is running in the current round.
 
 import sys
 import numpy as np
@@ -10,15 +10,15 @@ np.set_printoptions(threshold=sys.maxsize)
 
 def CDSMCreator(genusid):
     print '\ninputCDSM_creator.CDSMCreator running...'
-    mainfolder = "C:/Users/Balazs Dienes/PycharmProjects/SEBEphen/mainfolder/"
+    mainfolder = "... /"
 
     # input CDSM to numpy array
-    inputCDSM = r"mainfolder/input_CDSM/a1_cdsm.tif"
+    inputCDSM = r"... .tif"
     inputRaster = gdal.Open(inputCDSM)
     CDSMArray = np.array(inputRaster.GetRasterBand(1).ReadAsArray()).astype(np.float)
 
     # input vegtype to numpy array
-    inputVegtype = r"mainfolder/input_veg_type/a4_bet_frax.tif"
+    inputVegtype = r"... .tif"
     vegtypeRaster = gdal.Open(inputVegtype)
     vegtypeArray = np.array(vegtypeRaster.GetRasterBand(1).ReadAsArray())
 
