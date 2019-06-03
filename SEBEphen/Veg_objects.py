0037f407e9ca4b69f0a68a7dad81b079be9e7400
Balazs Dienes <dienes.balazs88@gmail.com>
2019-06-03 11:37:41 +0200

    Create Veg_objects.py

---
+++ SEBEphen/Veg_objects.py
@@ -0,0 +1,11 @@
+# Author: Balazs Dienes
+# Contact: dienes.balazs88@gmail.com
+
+# This file contains a class that defines object variables for each vegetation type.
+
+class vegobjects:
+    def __init__(self):
+        self.name = ""
+        self.vegtypeCDSM = 0
+        self.transmiss = 0
+        self.wallmatrix = 0
